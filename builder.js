c5ed161247455eacb713899bc0338735f2af20e7
Thomas Aiglstorfer <t.aiglstorfer@gmail.com>
2016-08-25 16:09:19 +0200

    added a webview for the google sfb-calendar

---
+++ builder.js
@@ -0,0 +1,16 @@
+{
+  "osx" : {
+    "title": "DIÖ Desktop Client",
+    "background": "assets/osx/background.png",
+    "icon": "assets/app-icon.ico",
+    "icon-size": 128,
+    "contents": [
+      { "x": 355, "y": 125, "type": "link", "path": "/Applications" },
+      { "x": 155, "y": 125, "type": "file" }
+    ]
+  },
+  "win" : {
+    "title" : "DIÖ Desktop Client",
+    "icon" : "assets/app-icon.ico"
+  }
+}
